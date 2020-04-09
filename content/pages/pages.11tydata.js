8fb321d59cf01640565ca287f6cfc292fe58a71a
Reuben L. Lillie <reubenlillie@gmail.com>
2020-04-09 15:02:45 -0500

    Update directory data files

--- content/pages/pages.11tydata.js
+++ content/pages/pages.11tydata.js
@@ -14,5 +14,7 @@ module.exports = {
   // Note: The permalink value uses Nunjucks/Liquid syntax;
   // a future version of 11ty may allow for JavaScript template literals
   permalink: '/{{page.fileSlug}}/index.html',
-  tags: 'pages'
+  tags: [
+    'pages'
+  ]
 }
--- content/posts/posts.11tydata.js
+++ content/posts/posts.11tydata.js
@@ -14,5 +14,7 @@ module.exports = {
   // Note: The permalink value uses Nunjucks/Liquid syntax;
   // a future version of 11ty may allow for JavaScript template literals
   permalink: '/{{site.en.postsArchive.url}}/{{page.fileSlug}}/index.html',
-  tags: 'posts'
+  tags: [
+    'posts'
+  ]
 }
