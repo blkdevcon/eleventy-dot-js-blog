2a924b59e1e5cca12fa70518d1f3d02285d4d38b
Reuben L. Lillie <reubenlillie@gmail.com>
2020-04-09 15:07:14 -0500

    Call archive shortcode in archive and home layouts

--- _includes/layouts/archive.11ty.js
+++ _includes/layouts/archive.11ty.js
@@ -6,6 +6,7 @@
 
 /**
  * Acts as front matter in JavaScript templates
+ * @see {@link https://www.11ty.dev/docs/languages/javascript/#optional-data-method Optional `data` in JavaScript templates in 11ty}
  */
 exports.data = {
   layout: 'layouts/base'
@@ -20,21 +21,13 @@ exports.data = {
  * @see {@link https://www.11ty.dev/docs/pagination/ Pagination in 11ty}
  */
 exports.render = function (data) {
-  var l10n = data.site[data.locale]
-  return `<header>
-    <h2>${data.title}</h2>
-  </header>
-  ${data.content}
-  <section id="archive">
-    <ul>
-    ${data.pagination.items.map(item =>
-      `<li>
-        <a href="${item.data.page.url}">${item.data.title}</a>
-        <time>${item.data.page.date
-          .toLocaleDateString(`${l10n.locale}`, l10n.dateOptions)}</time>
-      </li>`).join('')}
-    </ul>
-  </section>
+  return `<article>
+    <header>
+      <h2>${data.title}</h2>
+    </header>
+    ${data.content}
+    ${this.archive(data, data.pagination.items)}
+  </article>
   <footer>
     ${this.paginationNav(data)}
   </footer>`
--- _includes/layouts/home.11ty.js
+++ _includes/layouts/home.11ty.js
@@ -6,7 +6,7 @@
 
 /**
  * Acts as front matter in JavaScript templates
- * @see {@link https://www.11ty.dev/docs/data/ Using data in 11ty}
+ * @see {@link https://www.11ty.dev/docs/languages/javascript/#optional-data-method Optional `data` in JavaScript templates in 11ty}
  */
 exports.data = {
   layout: 'layouts/base'
@@ -20,19 +20,13 @@ exports.data = {
  * @return {String} The rendered template
  * @see {@link https://www.11ty.dev/docs/pagination/ Pagination in 11ty}
  */
-exports.render = data => {
-  var reversed = [...data.collections.posts.slice(-2)].reverse()
+exports.render = function (data) {
   var l10n = data.site[data.locale]
+  var reversed = [...data.collections.posts.slice(-2)].reverse()
   return `<article>
     ${data.content}
     <h2>${l10n.postsArchive.headline}</h2>
-    <ul>
-    ${reversed.map(item =>
-      `<li>
-        <a href="${item.data.page.url}">${item.data.title}</a>
-        <time>${item.data.page.date.toLocaleDateString(`${l10n.locale}`, l10n.dateOptions)}</time>
-      </li>`).join('')}
-    </ul>
+    ${this.archive(data, reversed)}
     <p><a href="${l10n.postsArchive.url}">${l10n.postsArchive.prompt}</a></p>
   </article>`
 }
